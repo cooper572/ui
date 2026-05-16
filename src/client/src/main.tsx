20f8f75acfbdda1cd427f83017ba71a3d87ce105
cooper572 <cooper@gisschool.org>
2026-05-16 20:09:43 +0700

    Restore SPA routes on refresh and rename app to Pandora

--- src/client/src/main.tsx
+++ src/client/src/main.tsx
@@ -4,6 +4,23 @@ import App from "@/app/App"
 import AppProviders from "@/app/AppProviders"
 import { AppSettingsProvider } from "@/app/providers/settings-provider.tsx"
 
+restoreSpaRoute()
+
+function restoreSpaRoute() {
+    const { hash, search } = window.location
+
+    if (!search.startsWith("?/")) {
+        return
+    }
+
+    const restoredPath = search
+        .slice(1)
+        .replace(/~and~/g, "&")
+        .replace(/^\/?/, "/")
+
+    window.history.replaceState({}, "", `${restoredPath}${hash}`)
+}
+
 createRoot(document.getElementById("root")!).render(
     <AppSettingsProvider>
         <AppProviders>
